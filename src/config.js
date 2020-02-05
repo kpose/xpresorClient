b3a876a162b105628bc6d296c9c8ff85fcdfbdd9
kpose <ganihujude@gmail.com>
2020-02-05 09:43:06 +0100

    Routing, Login with AWS, login component, navbar

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "xpresor-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://xkii1flkv5.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_qdpivx6cp",
+      APP_CLIENT_ID: "56ii7g7hljkms03fa8urb0sd7p",
+      IDENTITY_POOL_ID: "us-east-1:923b931b-4262-4a53-808b-bb05a8fecc86"
+    }
+  };
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+export default function Home() {
+    return (
+        <div className ='Home'>
+            <div className= 'lander'>
+                <h1>Xpresor</h1>
+                <p>Take your thoughts a step further</p>
+            </div>
+        </div>
+    )
+}
+
