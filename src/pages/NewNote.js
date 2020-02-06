fada41ff109ef86617a4ac735c27a00dc2e89f3d
kpose <ganihujude@gmail.com>
2020-02-06 08:28:07 +0100

    upload files to s3 bucket

--- src/pages/NewNote.js
+++ src/pages/NewNote.js
@@ -4,6 +4,9 @@ import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewNote.css";
 
+import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
+
 export default function NewNote(props) {
   const file = useRef(null);
   const [content, setContent] = useState("");
@@ -19,7 +22,7 @@ export default function NewNote(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+  
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
         `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
@@ -27,8 +30,25 @@ export default function NewNote(props) {
       );
       return;
     }
-
+  
     setIsLoading(true);
+  
+    try {
+      const attachment = file.current
+        ? await s3Upload(file.current)
+        : null;
+  
+      await createNote({ content, attachment });
+      props.history.push("/");
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
+    }
+  }
+  function createNote(note) {
+    return API.post("notes", "/notes", {
+      body: note
+    });
   }
 
   return (
